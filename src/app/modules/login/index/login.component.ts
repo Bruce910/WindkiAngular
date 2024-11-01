fad7e8e091a09f288ad0e279fd1b189fed826730
eugen <eugen@DESKTOP-848N6I2>
2024-11-01 12:33:25 +0800

    gen

--- src/app/modules/login/index/login.component.ts
+++ src/app/modules/login/index/login.component.ts
@@ -9,17 +9,8 @@ export class LoginComponent {
 logindisplay: string ='flex';
   closeForm(){
     this.logindisplay = 'none';
-    // const form = document.getElementById("myForm");
-    // if (form) {
-    //   form.style.display = "none";
-    // }
   }
   openForm(){
-    alert('openForm');
     this.logindisplay = 'flex';
-    // const form = document.getElementById("myForm");
-    // if (form) {
-    //   form.style.display = "flex";
-    // }
   }
 }
