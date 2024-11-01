51476a0c6e65379f7d24722024c7453ae8a5a6a1
eugen <eugen@DESKTOP-848N6I2>
2024-11-01 12:53:42 +0800

    temp

--- src/app/modules/login/index/login.component.ts
+++ src/app/modules/login/index/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { LoginPost } from './../../../services/login/interfaces/login.model';
+import { LoginService } from './../../../services/login/login.service';
+import { Router, Routes } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-logindisplay: string ='none';
+export class LoginComponent implements OnInit {
+  logindisplay: string ='none';
+  loginValue: LoginPost = {
+    AccountName: '',
+    Password: ''
+  }
+  constructor(private router:Router,private loginService:LoginService){}
+  ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  }
+  Login(){
+    this.router.navigateByUrl('');
+  }
   closeForm(){
     this.logindisplay = 'none';
   }
